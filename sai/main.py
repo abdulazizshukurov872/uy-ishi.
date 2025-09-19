62950e4bb79a296cff62998fb2411eb4e5986c12
abdulazizshukurov872 <abdulazizshukurov872@gmail.com>
2025-09-19 18:29:47 +0500

    Add files via upload

---
+++ sai/main.py
@@ -0,0 +1,14 @@
+print(21+56)
+print(665-43)
+print(43*54)
+print(976/54)
+ism="Saidansaf"
+familiya="Afzalxonov"
+yosh=101
+print(f'Mening ismim {ism} familiyam {familiya} yoshim {yosh} da')
+print(5674382/6483)
+p=3.14
+r=43
+print(p*r**2)
+print(32**2)
+print(32**3)
